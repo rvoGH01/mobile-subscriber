aa759a5ae21c58eb2260d91864e69abb25e98931
Roman.Voloshyn <roman.voloshyn@visiquate.com>
2019-04-01 01:06:53 +0300

    added logging

--- src/main/java/com/vodafone/mobile/controller/MobileSubscriberController.java
+++ src/main/java/com/vodafone/mobile/controller/MobileSubscriberController.java
@@ -3,6 +3,8 @@ package com.vodafone.mobile.controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,8 @@ import com.vodafone.mobile.service.MobileSubscriberService;
 @RestController
 @RequestMapping("/api/mobile-subscribers")
 public class MobileSubscriberController {
+    private final static Logger LOG = LoggerFactory.getLogger(MobileSubscriberController.class);
+
     private final MobileSubscriberService service;
 
     @Autowired
@@ -23,6 +27,7 @@ public class MobileSubscriberController {
 
     @GetMapping
     public List<MobileSubscriber> findAll(@RequestParam(required = false) String query) {
+        LOG.debug("GET /api/mobile-subscribers, query: '{}'", query);
         try {
             return service.findAll(query);
         } catch (Exception ex) {
@@ -33,8 +38,10 @@ public class MobileSubscriberController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public MobileSubscriber create(@RequestBody MobileSubscriber mobileSubscriber) {
+        LOG.debug("POST /api/mobile-subscribers, mobileSubscriber: '{}'", mobileSubscriber);
         Optional<MobileSubscriber> available = service.findByMsisdn(mobileSubscriber.getMsisdn());
         if (available.isPresent()) {
+            LOG.warn("Mobile subscriber with msisdn='{}' already exists.", mobileSubscriber.getMsisdn());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Mobile subscriber exists already");
         }
         try {
@@ -47,8 +54,10 @@ public class MobileSubscriberController {
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public MobileSubscriber update(@PathVariable Integer id, @RequestBody MobileSubscriber mobileSubscriber) {
+        LOG.debug("PUT /api/mobile-subscribers/{}, mobileSubscriber: '{}'", id, mobileSubscriber);
         Optional<MobileSubscriber> available = service.findById(id);
         if (!available.isPresent()) {
+            LOG.warn("There is no mobile subscriber with id='{}' to update.", id);
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Mobile subscriber not found");
         }
         mobileSubscriber.setId(id);
@@ -62,8 +71,10 @@ public class MobileSubscriberController {
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public void deleteById(@PathVariable("id") Integer id) {
+        LOG.debug("DELETE /api/mobile-subscribers/{}", id);
         Optional<MobileSubscriber> available = service.findById(id);
         if (!available.isPresent()) {
+            LOG.warn("There is no mobile subscriber with id='{}' to delete.", id);
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Mobile subscriber not found");
         }
         try {
@@ -76,8 +87,10 @@ public class MobileSubscriberController {
     @DeleteMapping
     @ResponseStatus(HttpStatus.OK)
     public void deleteByMsisdn(@RequestParam(required = true) String msisdn) {
+        LOG.debug("DELETE /api/mobile-subscribers?msisdn={}", msisdn);
         Optional<MobileSubscriber> available = service.findByMsisdn(msisdn);
         if (!available.isPresent()) {
+            LOG.warn("There is no mobile subscriber with msisdn='{}' to delete.", msisdn);
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Mobile subscriber not found");
         }
         try {
